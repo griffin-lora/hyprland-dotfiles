59554c5cc47d6731efee5fa7324e64c05b7dc1d9
griffin-lora <griffin-lora@github.com>
2024-05-02 21:00:52 -0400

    update some minor stuff

--- src/dotfiles_resources/scripts/launchpad.py
+++ src/dotfiles_resources/scripts/launchpad.py
@@ -1,11 +1,12 @@
 import subprocess
+import sys
 
 completed_proc = subprocess.run(["pgrep", "-l", "rofi"])
 
 if completed_proc.returncode == 1:
     subprocess.check_output(["eww", "open", "window-background"])
 
-    proc = subprocess.Popen(["rofi", "-show", "drun"])
+    proc = subprocess.Popen(["rofi", "-show", sys.argv[1]])
     proc.wait()
 
     subprocess.check_output(["eww", "close", "window-background"])
