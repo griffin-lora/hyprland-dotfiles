0ff4f23589143909da99ed0472c14dea801a8666
griffin-lora <griffin-lora@github.com>
2024-05-08 22:38:21 -0400

    add hiding waybar

--- src/dotfiles_resources/scripts/launchpad.py
+++ src/dotfiles_resources/scripts/launchpad.py
@@ -4,9 +4,11 @@ import sys
 completed_proc = subprocess.run(["pgrep", "-l", "rofi"])
 
 if completed_proc.returncode == 1:
+    subprocess.check_output(["eww", "open", "dropdown-animation"])
     subprocess.check_output(["eww", "open", "window-background"])
 
     proc = subprocess.Popen(["rofi", "-show", sys.argv[1]])
     proc.wait()
 
     subprocess.check_output(["eww", "close", "window-background"])
+    subprocess.check_output(["eww", "close", "dropdown-animation"])
